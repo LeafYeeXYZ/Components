557fa6238999539462ae2a3682d6fd0accf0c8b8
LeafYeeXYZ <leafyeexyz@outlook.com>
2025-06-13 21:18:23 +0800

    perf: add react-compiler

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,14 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [
+		react({
+			babel: {
+				plugins: [['babel-plugin-react-compiler', {}]],
+			},
+		}),
+		tailwindcss(),
+	],
 	build: {
 		target: ['chrome110', 'firefox115', 'safari16', 'edge110'],
 		chunkSizeWarningLimit: 600,

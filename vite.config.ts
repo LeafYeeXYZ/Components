1ed0cb9db4ec6f9a424f5374ab9d49c4f7db119e
LeafYeeXYZ <leafyeexyz@outlook.com>
2025-06-13 16:15:24 +0800

    perf: better chunking

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+	plugins: [react(), tailwindcss()],
+	build: {
+		target: ['chrome110', 'firefox115', 'safari16', 'edge110'],
+		chunkSizeWarningLimit: 600,
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					antd: ['antd', '@ant-design/icons'],
+					antv: ['@antv/g', '@antv/g-canvas'],
+				},
+			},
+		},
+	},
 })
